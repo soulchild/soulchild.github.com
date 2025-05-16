cf67cb09a0e6776fd7a6a6c79dbc02be4a21e02e
Tobias Kremer <tobias.kremer@dg-i.net>
2025-05-16 12:07:27 +0200

    refactor

---
+++ js/boombox.js
@@ -0,0 +1,46 @@
+export function Boombox() {
+  let source;
+  let audioBuffer;
+
+  async function fetchAudioBuffer() {
+    if (!audioBuffer) {
+      const arrayBuffer = await fetch("/audio/funkreich.ogg").then((res) =>
+        res.arrayBuffer()
+      );
+      const audioCtx = new window.AudioContext();
+      audioBuffer = await audioCtx.decodeAudioData(arrayBuffer);
+      audioCtx.close();
+    }
+    return audioBuffer;
+  }
+
+  async function toggleAudio() {
+    document.getElementById("paused").classList.toggle("hidden");
+    document.getElementById("playing").classList.toggle("hidden");
+
+    const audioBuffer = await fetchAudioBuffer();
+
+    if (source) {
+      source.stop();
+    } else {
+      let audioCtx = new window.AudioContext();
+      source = audioCtx.createBufferSource();
+      source.buffer = audioBuffer;
+      source.loop = true;
+      source.connect(audioCtx.destination);
+      source.onended = () => {
+        source.disconnect();
+        source = null;
+        if (audioCtx) {
+          audioCtx.close();
+          audioCtx = null;
+        }
+      };
+      source.start();
+    }
+  }
+
+  return {
+    toggleAudio,
+  };
+}
